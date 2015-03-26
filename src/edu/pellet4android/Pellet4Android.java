0aacd04284d8051173984b1515ca60404abfd560
edlectrico <edlectrico@gmail.com>
2015-03-26 11:11:29 +0100

    Refactoring

--- src/edu/pellet4android/Pellet4Android.java
+++ src/edu/pellet4android/Pellet4Android.java
@@ -5,19 +5,11 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Collection;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
-import org.semanticweb.owlapi.model.OWLLiteral;
-
-import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Environment;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 import edu.pellet4android.ontmanager.OntologyManager;
 import edu.pellet4android.ontmanager.exceptions.OntologyLoadException;
 
@@ -35,10 +27,6 @@ public class Pellet4Android {
 	
 	private static OntologyManager ontManager;
 	private String namespace;
-	private Collection<View> views;
-	
-	@SuppressWarnings("unused")
-	private List<String> buttons, editTexts, textViews;
 	
 	public Pellet4Android(){
 		ontManager = new OntologyManager();
@@ -56,7 +44,6 @@ public class Pellet4Android {
 		ontManager = new OntologyManager();
 		
 		this.namespace = namespace;
-		this.views = views;
 	}
 	
 	public void loadOntologyFromFile(final String path, final String ontologyFilename){
@@ -91,15 +78,8 @@ public class Pellet4Android {
 			//loaded yet. I recommend, for example, using a dialog, opening
 			//it when the loading process starts, and closing it in this
 			//method.
-			for (View v : views){
-				if (v instanceof Button){
-					buttons = ontManager.getIndividualOfClass(namespace + v.getClass().getSimpleName());
-				} else if (v instanceof EditText){
-					editTexts = ontManager.getIndividualOfClass(namespace + v.getClass().getSimpleName());
-				} else if (v instanceof TextView){
-					textViews = ontManager.getIndividualOfClass(namespace + v.getClass().getSimpleName());
-				}
-			}
+
+			// ontologyClassA = ontManager.getIndividualOfClass(namespace + clazz.getClass().getSimpleName());
 		}
 	}
 	
@@ -132,60 +112,5 @@ public class Pellet4Android {
 	 */
 	public List<String> getIndividualOfClass(final String namespace, final String clazz){
 		return ontManager.getIndividualOfClass(namespace + clazz);
-	}
-	
-	/**
-	 * This method requires a previous initialization of the
-	 * required views. To do so, the AdaptUI(final String namespace, 
-	 * final Collection<View> views) method is given. In the calling
-	 * activity the views should be added like this: views.add(button);
-	 */
-	public Map<String, Integer> adaptLoadedViews(){
-		Map<String, Integer> map = new HashMap<String, Integer>();
-		for (View v : views){
-			System.out.println(v.getClass().getSimpleName());
-			map.put("viewBackgroundColor", adaptViewBackgroundColor(this.namespace, v.getClass().getSimpleName()));
-			map.put("viewTextColor", adaptViewTextColor(this.namespace, v.getClass().getSimpleName()));
-			map.put("viewTextSize", adaptViewTextSize(this.namespace, v.getClass().getSimpleName()));
-		}
-		
-		return map;
-	}
-	
-	public int adaptViewBackgroundColor(final String namespace, final String className) {
-		List<String> genericViews = ontManager.getIndividualOfClass(namespace + className);
-		
-		final Collection<OWLLiteral> backgroundColor = ontManager.getDataTypePropertyValue(genericViews.get(0), namespace + "viewHasColor");
-		final int color = Integer.parseInt(((OWLLiteral) backgroundColor.toArray()[0]).getLiteral());
-		
-		//convert color to RGB
-		final int red = Color.red(color);
-		final int green = Color.green(color);
-		final int blue = Color.blue(color);
-		
-		return Color.argb(255, red, green, blue);
-	}
-	
-	public int adaptViewTextColor(final String namespace, final String className){
-		List<String> genericViews = ontManager.getIndividualOfClass(namespace + className);
-		
-		final Collection<OWLLiteral> textColor = ontManager.getDataTypePropertyValue(genericViews.get(0), namespace + "viewHasTextColor");
-		final int viewTextColor = Integer.parseInt(((OWLLiteral) textColor.toArray()[0]).getLiteral());
-		
-		//convert color to RGB
-		final int red = Color.red(viewTextColor);
-		final int green = Color.green(viewTextColor);
-		final int blue = Color.blue(viewTextColor);
-		
-		return Color.argb(255, red, green, blue);
-	}
-	
-	public int adaptViewTextSize(final String namespace, final String className){
-		List<String> genericViews = ontManager.getIndividualOfClass(namespace + className);
-		
-		final Collection<OWLLiteral> textSize = ontManager.getDataTypePropertyValue(genericViews.get(0), namespace + "viewHasTextSize");
-		final float viewTextSize = Float.parseFloat(((OWLLiteral) textSize.toArray()[0]).getLiteral());
-		
-		return (int)viewTextSize;
 	}
 }
